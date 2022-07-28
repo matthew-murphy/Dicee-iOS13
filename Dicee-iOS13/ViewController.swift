cf0de365390cad244e1ac77673a5e4ed5dd5ee81
Matt Murphy <matt.murphy@blindrobot.com>
2022-07-28 12:04:56 -0400

    dice roll feature implemented

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,20 +1,17 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+        
+        let diceArray = [UIImage(named:"DiceOne"), UIImage(named:"DiceTwo"), UIImage(named:"DiceThree"), UIImage(named:"DiceFour"), UIImage(named:"DiceFive"), UIImage(named:"DiceSix")]
+                        
+        diceImageViewOne.image = diceArray[Int.random(in: 1...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 1...5)]
+        
     }
-
-
 }
 
